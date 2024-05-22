52001cedc6bbb4d76feb3e1228d14b2b7abdf90e
JerrySC68 <jerrysc68@gmail.com>
2024-05-22 13:06:13 -0600

    Cambios listaFacturas

--- proyecto2-frontend/src/Components/Factura/ListFactura.js
+++ proyecto2-frontend/src/Components/Factura/ListFactura.js
@@ -1,9 +1,51 @@
 import React, { useState, useEffect } from 'react';
 import FacturaService from "../../Services/FacturaService";
-import { Link } from 'react-router-dom';
-import CreateFactura from "./CreateFactura";
+import { Link } from 'react-router-dom'
 
-export const ListProductos = () => {
+export const ListFacturas = () => {
+    const [facturas, setFacturas] = useState([]);
 
+    useEffect(() => {
+        FacturaService.getFacturas().then((response) => {
+            setFacturas(response.data);
+        }).catch((error) => {
+            console.log(error);
+        });
+    }, []);
+
+    return (
+        <div className='tabla-productos'>
+            <h2>Facturas</h2>
+            <table>
+                <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Fecha</th>
+                    <th>Proveedor</th>
+                    <th>Cliente</th>
+                    <th>Tipo_Pago</th>
+                    <th>Precio_Final</th>
+                </tr>
+                </thead>
+                <tbody>
+                {
+                    facturas.map(
+                        factura =>
+                            <tr key={factura.id}>
+                                <td>{factura.id}</td>
+                                <td>{factura.date}</td>
+                                <td>{factura.cedulaProveedor}</td>
+                                <td>{factura.cedulaCliente}</td>
+                                <td>{factura.tipoPago}</td>
+                                <td>{factura.finalPrice}</td>
+                            </tr>
+                    )
+                }
+                </tbody>
+            </table>
+            <Link to="/save-productos">Agregar Factura</Link>
+        </div>
+    );
 }
-export default ListProductos;
+
+export default ListFacturas;
