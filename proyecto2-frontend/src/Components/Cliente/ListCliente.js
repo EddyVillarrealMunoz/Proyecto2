5904974e39969dcccc4b6a1183fd6fd59641f281
RodrigoACR <rodrigo.castro.rojas@est.una.ac.cr>
2024-05-22 14:53:56 -0600

    Cliente List Mascara, Opciones Bootstrap

--- proyecto2-frontend/src/Components/Cliente/ListCliente.js
+++ proyecto2-frontend/src/Components/Cliente/ListCliente.js
@@ -1,12 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React, {useEffect, useState} from 'react';
 import ClienteService from "../../Services/ClienteService";
-import { Link } from 'react-router-dom';
-
+import {Link, useNavigate} from 'react-router-dom';
 import '../../css/style.css';
 
 export const ListClientes = () => {
+    //------------------------------------------------------------------------------------------------------------------
+    // CONSTANTES
+    //------------------------------------------------------------------------------------------------------------------
     const [clientes, setClientes] = useState([]);
+    const navigator = useNavigate();
 
+    //------------------------------------------------------------------------------------------------------------------
+    // Obtener Data
+    //------------------------------------------------------------------------------------------------------------------
     useEffect(() => {
         ClienteService.getClientes().then((response) => {
             setClientes(response.data);
@@ -15,35 +21,89 @@ export const ListClientes = () => {
         });
     }, []);
 
+    //------------------------------------------------------------------------------------------------------------------
+    // FUNCIONES
+    //------------------------------------------------------------------------------------------------------------------
+    function addCliente() {
+        console.log("Agregar Cliente");
+        navigator("/save-clientes");
+    }
+
+    function removeCliente(id) {
+        console.log("Eliminar cliente con id: ", id);
+
+        ClienteService.deleteCliente(id).then(() => {
+            ClienteService.getClientes();
+        }).catch(error => {
+            console.error("Error al eliminar el cliente: ", error);
+        })
+    }
+
+    function formatoCedula(tipoCliente, cedula) {
+        console.log(tipoCliente, cedula)
+        if (tipoCliente === true && cedula.length === 9) {
+            return `${cedula.slice(0, 1)}-${cedula.slice(1,5)}-${cedula.slice(5)}`;
+        }
+        if(tipoCliente === false && cedula.length === 10)
+        {
+            return `${cedula.slice(0, 1)}-${cedula.slice(1,4)}-${cedula.slice(5)}`;
+        }
+
+        return cedula;
+    }
+
+    function formatoTelefono(telefono) {
+        if (telefono.length === 8) {
+            return `${telefono.slice(0, 4)}-${telefono.slice(4)}`;
+        }
+
+        return telefono;
+    }
+
+    //------------------------------------------------------------------------------------------------------------------
+    // RENDERIZADO
+    //------------------------------------------------------------------------------------------------------------------
     return (
         <div className='tabla-Cliente'>
             <h2>Clientes</h2>
+            <button className="btn btn-primary mb-1 float-end" onClick={addCliente}>Nuevo Cliente</button>
             <table>
                 <thead>
-                <tr>
-                    <th>Tipo Cliente</th>
-                    <th>Nombre</th>
-                    <th>Dirección</th>
-                    <th>Teléfono</th>
-                    <th>Email</th>
-                </tr>
+                    <tr>
+                        <th>Cedula</th>
+                        <th>Tipo Cliente</th>
+                        <th>Nombre</th>
+                        <th>Dirección</th>
+                        <th>Teléfono</th>
+                        <th>Email</th>
+                        <th>Accion</th>
+                    </tr>
                 </thead>
                 <tbody>
-                {
-                    clientes.map(
-                        cliente =>
-                            <tr key={cliente.id}>
-                                <td>{cliente.tipoCliente ? "Físico" : "Jurídico"}</td>
-                                <td>{cliente.name}</td>
-                                <td>{cliente.direccion}</td>
-                                <td>{cliente.telefono}</td>
-                                <td>{cliente.email}</td>
-                            </tr>
-                    )
-                }
+                    {
+                        clientes.map(
+                            cliente =>
+                                <tr key={cliente.id}>
+                                    <td>{formatoCedula(cliente.tipoCliente, cliente.id)}</td>
+                                    <td>{cliente.tipoCliente ? "Físico" : "Jurídico"}</td>
+                                    <td>{cliente.name}</td>
+                                    <td>{cliente.direccion}</td>
+                                    <td>{formatoTelefono(cliente.telefono)}</td>
+                                    <td>{cliente.email}</td>
+                                    <td>
+                                        <div className={"text-center"}>
+                                            <button className="btn btn-info m-1">Ver</button>
+                                            <button className="btn btn-danger m-1"
+                                                    onClick={() => removeCliente(cliente.id)}>
+                                                Eliminar
+                                            </button>
+                                        </div>
+                                    </td>
+                                </tr>
+                        )
+                    }
                 </tbody>
             </table>
-            <Link to="/save-clientes">Agregar Clientes</Link>
         </div>
     );
 }
