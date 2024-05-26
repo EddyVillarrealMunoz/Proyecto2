3675a2a9a57b033acb3b04696e387736f691e1ba
EddyVillarrealMunoz <eddy.villarreal.munoz@est.una.ac.cr>
2024-05-26 08:20:32 -0600

    Cambios Eddy 12

--- proyecto2-frontend/src/Components/Header.js
+++ proyecto2-frontend/src/Components/Header.js
@@ -1,9 +1,29 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
 import '../css/style.css';
+import { Link, useNavigate } from 'react-router-dom';
 import facturaImage from '../images/factura.png';
 
-export const Header = () => {
+const Header = () => {
+    const [user, setUser] = useState(null);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const loggedInUser = localStorage.getItem('user');
+        if (loggedInUser) {
+            setUser(JSON.parse(loggedInUser));
+        }
+    }, []);
+
+    const handleProfileRedirect = () => {
+        if (user) {
+            if (user.rol === 'ADM') {
+                navigate('/admin-profile');
+            } else if (user.rol === 'PRO') {
+                navigate(`/profile-proveedor/${user.id}`);
+            }
+        }
+    }
+
     return (
         <div>
             <header>
@@ -14,10 +34,12 @@ export const Header = () => {
                     <a href="/facturas">Facturas</a>
                     <a href="/productos">Productos</a>
                     <a href="/clientes">Clientes</a>
+                    <a href="#" onClick={handleProfileRedirect}>Mi Perfil</a>
                     <a href="/logout">Salir</a>
                 </nav>
             </header>
         </div>
     )
 }
+
 export default Header;
